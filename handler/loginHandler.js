eec505b481e90dec09c3ea7d3a361ff8a6f45cf5
k15 <krisnantobiyuh@gmail.com>
2022-04-21 12:41:23 +0700

    Fix undefined variable

--- handler/loginHandler.js
+++ handler/loginHandler.js
@@ -4,15 +4,14 @@ import { setToken } from '../store/state.js'
     const btnLogin = document.getElementById('btn-login')
 
     const eventLoginBtn = () => {
-
-        const user     = document.getElementById('username').value
-        const password = document.getElementById('password').value
         const token    = document.getElementById('token').value
 
         if (token) {
             setToken(token)
             alert(`User added to socket : ${token}`)
         } else {
+            const user     = document.getElementById('username').value
+            const password = document.getElementById('password').value
             const options = {
                 method: "POST",
                 headers: {
